80f891c3e85fa85f1c0ae88affdc9ea2c97ab2eb
moriconic17 <Christopher@Christophers-MacBook-Air.local>
2016-04-21 10:34:53 -0400

    Simple GUI Test

--- src/application/MainWindowController.java
+++ src/application/MainWindowController.java
@@ -1,5 +1,31 @@
 package application;
-
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+//This is a change
 public class MainWindowController {
+	int screenNumber = 0;
 	
+    @FXML
+    private Button buttonDown;
+
+    @FXML
+    private Button buttonUp;
+
+    @FXML
+    private Label screen;
+
+    @FXML
+    void onClick_up(ActionEvent event) {
+    	screenNumber++;
+    	screen.setText(new String(""+screenNumber));
+    }
+
+    @FXML
+    void onClick_down(ActionEvent event) {
+    	screenNumber--;
+    	screen.setText(new String(""+screenNumber));
+    }
+
 }
