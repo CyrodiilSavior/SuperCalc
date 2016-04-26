f370a2108165e0b7f8dfdd5820a2e0b7dd54d157
moriconic17 <Christopher@Christophers-MacBook-Air.local>
2016-04-26 14:08:42 -0400

    hi

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,10 +13,11 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,400,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			primaryStage.setResizable(false);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
